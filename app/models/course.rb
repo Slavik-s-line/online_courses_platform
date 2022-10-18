beedc7354c132c9a681e79178b010fdcf0045b16
Slava Sukhenko <slavichmy@gmail.com>
2022-10-18 23:00:58 +0300

    gem ransack for Courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -11,4 +11,14 @@ class Course < ApplicationRecord
   has_rich_text :description
   extend FriendlyId
   friendly_id :title, use: :slugged
+  
+  LANGUAGES = [:"English", :"Russian", :"Polish", :"Spanish"]
+  def self.languages
+    LANGUAGES.map { |language| [language, language] }
+  end
+
+  LEVELS = [:"Beginner", :"Intermediate", :"Advanced"]
+  def self.levels
+    LEVELS.map { |level| [level, level] }
+  end
 end
