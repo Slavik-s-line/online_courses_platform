0502c2922470e8a49b6aa4533ea3fb829d59e869
Slava Sukhenko <slavichmy@gmail.com>
2022-10-19 22:00:31 +0300

    gem pundit - installation and example usecase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include PublicActivity::StoreController
+  include Pundit
+  protect_from_forgery
   before_action :set_global_variables, if: :user_signed_in?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
   end
 end
